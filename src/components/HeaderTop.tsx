30fe40c1ad52981fdc86e832c6417598f4f17d3f
clymo <jahnnho.moraga@gmail.com>
2023-10-06 23:50:26 -0300

    [ADD] se agregaron nuevos campos

--- src/components/HeaderTop.tsx
+++ src/components/HeaderTop.tsx
@@ -170,13 +170,13 @@ export default function HeaderTop() {
                                     href="#"
                                     className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                                 >
-                                    Features
+                                    Actualizaciones
                                 </a>
                                 <a
                                     href="#"
                                     className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                                 >
-                                    Marketplace
+                                    Tienda 
                                 </a>
                                 <a
                                     href="#"
@@ -190,7 +190,7 @@ export default function HeaderTop() {
                                     href="#"
                                     className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                                 >
-                                    Log in
+                                    Ingresar
                                 </a>
                             </div>
                         </div>
